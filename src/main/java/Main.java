5317c30ba17053e0b84fca68c4df8bf97d9ed7fa
DakMcMak <esbolov.galymzhan@mail.ru>
2017-03-04 22:52:21 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int numberE = scanner.nextInt();
+       switch(numberE) {
+            case 25: System.out.println("2.7182818284590452353602875"); break;
+            case 24: System.out.println("2.718281828459045235360288"); break;
+            case 23: System.out.println("2.71828182845904523536029"); break;
+            case 22: System.out.println("2.7182818284590452353603"); break;
+            case 21: System.out.println("2.718281828459045235360"); break;
+            case 20: System.out.println("2.71828182845904523536"); break;
+            case 19: System.out.println("2.7182818284590452354"); break;
+            case 18: System.out.println("2.718281828459045235"); break;
+            case 17: System.out.println("2.71828182845904524"); break;
+            case 16: System.out.println("2.7182818284590452"); break;
+            case 15: System.out.println("2.718281828459045"); break;
+            case 14: System.out.println("2.71828182845905"); break;
+            case 13: System.out.println("2.7182818284590"); break;
+            case 12: System.out.println("2.718281828459"); break;
+            case 11: System.out.println("2.71828182846"); break;
+            case 10: System.out.println("2.7182818285"); break;
+            case 9: System.out.println("2.718281828"); break;
+            case 8: System.out.println("2.71828183"); break;
+            case 7: System.out.println("2.7182818"); break;
+            case 6: System.out.println("2.718282"); break;
+            case 5: System.out.println("2.71828"); break;
+            case 4: System.out.println("2.7183"); break;
+            case 3: System.out.println("2.718"); break;
+            case 2: System.out.println("2.72"); break;
+            case 1: System.out.println("2.7"); break;
+            default: System.out.println("3"); break;
+        }
+    }
+}
